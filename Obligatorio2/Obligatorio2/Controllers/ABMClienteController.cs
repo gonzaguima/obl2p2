b095c59a717b691732efc880c3b65c7bf33e0fb1
pingold16 <p.ingold@outlook.com>
2018-11-19 18:22:21 -0300

    Creacion de Vistas

---
+++ Obligatorio2/Obligatorio2/Controllers/ABMClienteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Obligatorio2.Controllers
+{
+    public class ABMClienteController : Controller
+    {
+        // GET: ABMCliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Alta() { return View(); }
+        public ActionResult Baja() { return View(); }
+        public ActionResult Mod() { return View(); }
+    }
+}
---
+++ Obligatorio2/Obligatorio2/Controllers/ListadoClientesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Obligatorio2.Controllers
+{
+    public class ListadoClientesController : Controller
+    {
+        // GET: ListadoClientes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
