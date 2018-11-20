09195e2bbac80d1a7c0bb1a94107d304f6e46845
pingold16 <p.ingold@outlook.com>
2018-11-20 17:32:16 -0300

    Vista Alta Cliente

--- Obligatorio2/Obligatorio2/Controllers/ABMClienteController.cs
+++ Obligatorio2/Obligatorio2/Controllers/ABMClienteController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ObligatorioDominio;
 
 namespace Obligatorio2.Controllers
 {
@@ -16,5 +17,19 @@ namespace Obligatorio2.Controllers
         public ActionResult Alta() { return View(); }
         public ActionResult Baja() { return View(); }
         public ActionResult Mod() { return View(); }
+        [HttpPost]
+        public ActionResult Alta(string nombre, string apellido, string documento, string direccion, int telefono, string user, string pass)
+        {
+            if (Sistema.Instancia.AltaCliente(nombre, apellido, documento, direccion, telefono, user, pass))
+            {
+                ViewBag.resultado("Alta exitosa");
+            }
+            else { ViewBag.resultado("Alta fallida"); }
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult Baja() { return View(); }
+        //[HttpPost]
+        //public ActionResult Mod() { return View(); }
     }
 }
