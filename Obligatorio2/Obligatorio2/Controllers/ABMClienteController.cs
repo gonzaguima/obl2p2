a39e1d84510a3d1060ccb49900f381422116d945
pingold16 <p.ingold@outlook.com>
2018-11-26 20:33:16 -0300

    Armado de Mod Cliente

--- Obligatorio2/Obligatorio2/Controllers/ABMClienteController.cs
+++ Obligatorio2/Obligatorio2/Controllers/ABMClienteController.cs
@@ -15,23 +15,21 @@ namespace Obligatorio2.Controllers
             return View();
         }
         public ActionResult Alta() { return View(); }
-        public ActionResult Baja()
-        {
-            return View();
-        }
+        public ActionResult Baja() { return View(); }
         public ActionResult Mod() { return View(); }
         [HttpPost]
         public ActionResult Alta(string nombre, string apellido, string documento, string direccion, int telefono, string user, string pass)
         {
-
+            //Recibe los datos del formulario 
             if (nombre != "" && apellido != "" && documento != "" && direccion != "" && telefono != 0 && pass != "" && user != "")
-            {
+            { //Verifica que no sean vacios
                 if (Sistema.Instancia.AltaCliente(nombre, apellido, documento, direccion, telefono, user, pass))
                 {
                     ViewBag.resultado = "Alta exitosa";
                 }
                 else { ViewBag.resultado = "Alta fallida"; }
             }
+            else { ViewBag.resultado = "Los campos son obligatorios"; }
             return View("Alta");
         }
         [HttpPost]
@@ -43,7 +41,18 @@ namespace Obligatorio2.Controllers
             }
             return View("Baja");
         }
-        //[HttpPost]
-        //public ActionResult Mod() { return View(); }
+        [HttpPost]
+        public ActionResult Mod(string borrador)
+        {
+            Cliente c = Sistema.Instancia.BuscarCliente(borrador);
+            @ViewBag.nombre = c.Nombre;
+            @ViewBag.apellido = c.Apellido;
+            @ViewBag.documento = c.Documento;
+            @ViewBag.direccion = c.Direccion;
+            @ViewBag.telefono = c.Telefono;
+            @ViewBag.user = c.User;
+            @ViewBag.pass = c.Pass;
+            return View();
+        }
     }
 }
